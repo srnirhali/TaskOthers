6e460d5800916f03b55b382e881488f6257d491c
Shivam Nirhali <srnirhali369@gmail.com>
2019-11-08 15:26:52 +0100

    Updated newsdata dump
    reset the auto-increment

--- newsdatabase_news.sql
+++ newsdatabase_news.sql
@@ -36,7 +36,7 @@ CREATE TABLE `news` (
   `content` text,
   `bodycontent` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
